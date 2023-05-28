8bcdd1a56087a625e7fba8c5084badac36e5a0e4
Madjar Ivan <evanmadjar@gmail.com>
2023-05-28 12:45:08 +0300

    test: Update User models tests

--- server/apps/users/tests/test_models.py
+++ server/apps/users/tests/test_models.py
@@ -1,13 +1,25 @@
 from django.test import TestCase
-from mixer.backend.django import mixer
 from users.models import User
 
 
-class TestUserModel(TestCase):
-    def setUp(self) -> None:
-        self.user = User.objects.create_user(
-            'user','user@user.com', '12345')
-    
-    def test_creation(self) -> None:
-        self.assertEqual(str(self.user), 'user')
-        self.assertEqual(self.user.description, None)
+class TestUsers(TestCase):
+    def test_user_model(self):
+        user: User = User.objects.create_user(
+            username='test', email='test@test.com', password='testpassword')
+        query: User = User.objects.first()
+
+        self.assertIsInstance(user, User)
+        self.assertEqual(query, user)
+        self.assertFalse(query.is_verified)
+        self.assertFalse(query.is_superuser)
+        self.assertFalse(query.is_staff)
+
+    def test_user_model_as_superuser(self):
+        user: User = User.objects.create_superuser(
+            username='test', email='test@test.com', password='testpassword')
+        query: User = User.objects.first()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user, query)
+        self.assertTrue(query.is_verified)
+        self.assertTrue(query.is_superuser)
+        self.assertTrue(query.is_staff)
